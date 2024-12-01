7ea10a8ba5bb7c34c5e324e8b7e20da86c6e2d90
StevicMilica <stevicmilica.kg99@gmail.mo>
2024-12-01 22:02:08 +0100

    Uslovi

---
+++ 6 Uslovi i grananje/1 Uslov/main.js
@@ -0,0 +1,11 @@
+
+//IF uslov
+const godine = 19
+if(godine >= 18){
+    console.log('Mozes pristupiti sajtu');
+}
+
+//interakcija sa korisnikm
+const godine1 = prompt("Unesite broj godina: ")
+if(godine1 >= 18)
+    document.write('Mozete pristupiti sajtu!');
